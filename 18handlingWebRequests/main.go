63e0706a3018beeeacd470a11e3548fc1122d6b8
Aryan019 <aryan19v@gmail.com>
2025-01-01 18:33:54 +0530

    Hitting get requests and handling in responses in go

---
+++ 18handlingWebRequests/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+// See the notes once
+func main() {
+
+	url := "https://google.com"
+	fmt.Println(url)
+	// http package will be used to hit a get requests to an entered website
+
+	fmt.Println("Web Requests in Go ")
+
+	response, err := http.Get(url)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("The response is %T\n", response)
+
+	// and the response body must must be closed
+	defer response.Body.Close()
+
+	// Reading in that response
+	databytes, err := io.ReadAll(response.Body)
+
+	// Converting it to the string
+	content := string(databytes)
+	fmt.Println(content)
+}
