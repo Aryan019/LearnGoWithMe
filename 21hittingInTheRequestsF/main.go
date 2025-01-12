11ab9f2f64374b7bf3153c1c1a2ac03a7d78cefa
Aryan019 <aryan19v@gmail.com>
2025-01-12 15:44:17 +0530

    Sending in the post and the postForm request

--- 21hittingInTheRequestsF/main.go
+++ 21hittingInTheRequestsF/main.go
@@ -9,11 +9,14 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"net/url"
+	"strings"
 )
 
 func main() {
 
 	fmt.Println("Hitting in the requests in go lang ")
+	performGetRequests()
 }
 
 func performGetRequests() {
@@ -44,5 +47,67 @@ func performGetRequests() {
 
 	// Making in the post request with go lang
 	// Assume in the endpoint we are sending in the data only accepts in the json format only
+
+}
+
+func performPostRequestsWithJson() {
+	// The url where you want to send in the post request
+
+	const myurl = "<enter the url here>"
+
+	//  Creating in the fake json payload
+	// after applying back ticks create in any kind of data
+	requestBody := strings.NewReader(`
+	"name" : "Aryan019",
+	"place": "Indore"
+	`)
+
+	// Actually sending in the post request
+	// While sending in post req three types of data to be included in
+	// the url you are sending it to
+	// the type of data you are sending in
+	// and the request body
+	response, err := http.Post(myurl, "application/json", requestBody)
+
+	if err != nil {
+		panic(err)
+	}
+
+	//Reading in the response
+	defer response.Body.Close()
+	content, _ := io.ReadAll(response.Body)
+	fmt.Println(string(content))
+
+	// Sending in the form data in go lang
+	// Useful in the case of image upload and all
+
+	//Performing in the post requests of the form
+
+}
+
+// Sending in the form data as in the request
+func performFormRequest() {
+	const myUrl = "<Some random url in which you can send in the form data"
+	// Creating in the fake form data
+
+	// This url package is used to create in the form data
+	data := url.Values{}
+
+	data.Add("name", "Aryan019")
+	data.Add("place", "Indore")
+
+	// PostForm method is used to sending in the form data request in go
+	// Also handling in the response in here
+	response, err := http.PostForm(myUrl, data)
+
+	if err != nil {
+		panic(err)
+
+	}
+
+	// Reading in the response
+	defer response.Body.Close()
+	content, _ := io.ReadAll(response.Body)
+	fmt.Println(string(content))
 
 }
