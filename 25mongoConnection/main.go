50e1ad841a3bea13eadc97a3ba49b572b838fa4b
Aryan019 <aryan19v@gmail.com>
2025-01-13 14:31:03 +0530

    Here we will organize the files in the correct project structure and will establish the connectivity to the mongo db

---
+++ 25mongoConnection/main.go
@@ -0,0 +1,18 @@
+// In the root directory of the project
+// There can only exist in one main.go file in the project
+// The main.go file is the entry point of the application
+// So files other than that will exist in the subfolder of the project
+
+package main
+
+import "fmt"
+
+// Here we will be working on
+// the mongo connection in go lang
+// via the mongoDb native driver only
+
+func main() {
+
+	fmt.Println("MongoDB in go lang")
+	fmt.Println("Will be natively only using in the mongo drivers only :-) ")
+}
